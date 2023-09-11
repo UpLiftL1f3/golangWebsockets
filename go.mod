741b810e019a7ea0e0b8036f9c36ab7f49c0b944
personal <upLiftL1f3@gmail.com>
2023-09-11 09:13:26 -0700

    init

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/UpLiftL1f3/tollCalc
+
+go 1.20
+
+require github.com/gorilla/websocket v1.5.0 // indirect
---
+++ obu/main.go
@@ -0,0 +1,64 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"math"
+	"math/rand"
+	"time"
+
+	"github.com/UpLiftL1f3/tollCalc/types"
+	"github.com/gorilla/websocket"
+)
+
+const wsEndpoint = "ws://127.0.0.1:30000/ws"
+
+var sendInterval = time.Second
+
+func genLatLong() (float64, float64) {
+	return genCoord(), genCoord()
+}
+
+func genCoord() float64 {
+	n := float64(rand.Intn(100 + 1))
+	f := rand.Float64()
+	return n + f
+}
+
+func generateOBUIDS(n int) []int {
+	ids := make([]int, n)
+	for i := 0; i < n; i++ {
+		ids[i] = rand.Intn(math.MaxInt)
+	}
+	return ids
+}
+
+func main() {
+	obuIDS := generateOBUIDS(25)
+	conn, _, err := websocket.DefaultDialer.Dial(wsEndpoint, nil)
+	if err != nil {
+		log.Fatal(err)
+	}
+	for {
+		for i := 0; i < len(obuIDS); i++ {
+			lat, long := genLatLong()
+			data := types.OBUData{
+				OBUID: obuIDS[i],
+				Lat:   lat,
+				Long:  long,
+			}
+			fmt.Printf("%+v\n", data)
+			if err := conn.WriteJSON(data); err != nil {
+				log.Fatal(err)
+			}
+
+		}
+		time.Sleep(sendInterval)
+	}
+}
+
+func init() {
+	rand.Seed(time.Now().UnixNano())
+	// seed := time.Now().UnixNano()
+	// rng := rand.New(rand.NewSource(seed))
+}
